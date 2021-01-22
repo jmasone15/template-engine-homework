c0180ab78a1c2b8b95ef1b66dfeed0c7d7427cbc
Jordan Masone <jordanmasone1@gmail.com>
2021-01-21 19:35:26 -0500

    Added else statement to close process.

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Left To Do: 
+// 1. Edit CSS of team.html page.
+// 2. Validate inquirer inputs.
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -49,7 +53,7 @@ function appMenu() {
                 console.log("Let's make an Engineer");
                 createEngineer();
             } else {
-                console.log("Goodbye");
+                createTeam(employeesArray);
             }
         })
     }
@@ -89,7 +93,7 @@ function appMenu() {
                 console.log("Let's make an Intern");
                 createIntern();
             } else {
-                console.log("Goodbye");
+                createTeam(employeesArray);
             }
         })
     }
@@ -136,30 +140,3 @@ function appMenu() {
     createManager();
 }
 appMenu();
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
-
-
-//seperate function for building team
-//inside of function use fs method to write to file
-//that fs method take in three parameters, (filepath, render function, data encoding "utf-8")
