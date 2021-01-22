82421117405c079896c9ab8cf4f6e5373dd5e23c
Jordan Masone <jordanmasone1@gmail.com>
2021-01-21 19:38:40 -0500

    Added comments to app.js.

--- app.js
+++ app.js
@@ -2,22 +2,24 @@
 // 1. Edit CSS of team.html page.
 // 2. Validate inquirer inputs.
 
+// Require all necessary js files and modules.
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
 const employeesArray = [];
 
+// Function to start up the application.
 function appMenu() {
     console.log("Please build your team...");
+
+    // Function that builds the manager object.
     function createManager() {
         inquirer.prompt([
             {
@@ -58,6 +60,7 @@ function appMenu() {
         })
     }
 
+    // Function that builds the engineer object.
     function createEngineer() {
         inquirer.prompt([
             {
@@ -98,6 +101,7 @@ function appMenu() {
         })
     }
 
+    // Function that builds the intern object.
     function createIntern() {
         inquirer.prompt([
             {
@@ -128,6 +132,7 @@ function appMenu() {
         })
     }
 
+    // Function that takes in the employee objects and renders them to HTML and writes it on the page.
     function createTeam(array) {
         fs.writeFile(outputPath, render(array), "utf-8", (err) => {
             if (err) throw err;
