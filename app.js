060b19b10b3ca4d47857a815152637d8e4bfea9c
Jordan Masone <jordanmasone1@gmail.com>
2021-01-22 19:23:58 -0500

    Added validation functions for the inqurier.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // Left To Do: 
 // 1. Edit CSS of team.html page.
-// 2. Validate inquirer inputs.
 
 // Require all necessary js files and modules.
 const Manager = require("./lib/Manager");
@@ -13,11 +12,34 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
+const { type } = require("os");
 const employeesArray = [];
 
 // Function to start up the application.
 function appMenu() {
     console.log("Please build your team...");
+
+    // Validation functions.
+    function validateName(name) {
+        var validName = /^[a-z ,.'-]+$/i;
+        return validName.test(name) || console.log(" Please enter a valid name");
+    };
+    function validateNum(num) {
+        var validNum = /^\d+$/;
+        return validNum.test(num) || console.log("  Please enter a valid number");
+    };
+    function validateEmail(email) {
+        var validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return validEmail.test(email) || console.log("  Please enter a valid email.");
+    };
+    function validateGithub(github) {
+        var validGithub = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/;
+        return validGithub.test(github) || console.log("Please enter a valid Github link.");
+    };
+    function validateSchool(school) {
+        var validSchool = /^[a-z ,.'-]+$/i;
+        return validSchool.test(school) || console.log(" Please enter a valid school name");
+    };
 
     // Function that builds the manager object.
     function createManager() {
@@ -25,22 +47,26 @@ function appMenu() {
             {
                 type: "input",
                 name: "managerName",
-                message: "What is your manager's name?"
+                message: "What is your manager's name?",
+                validate: validateName,
             },
             {
                 type: "input",
                 name: "managerId",
-                message: "What is your manager's id?"
+                message: "What is your manager's id?",
+                validate: validateNum,
             },
             {
                 type: "input",
                 name: "managerEmail",
-                message: "What is your manager's email?"
+                message: "What is your manager's email?",
+                validate: validateEmail,
             },
             {
                 type: "input",
                 name: "managerOfficeNumber",
-                message: "What is your manager's office number?"
+                message: "What is your manager's office number?",
+                validate: validateNum,
             },
             {
                 type: "confirm",
@@ -66,22 +92,26 @@ function appMenu() {
             {
                 type: "input",
                 name: "engineerName",
-                message: "What is your engineer's name?"
+                message: "What is your engineer's name?",
+                validate: validateName,
             },
             {
                 type: "input",
                 name: "engineerId",
-                message: "What is your engineer's id?"
+                message: "What is your engineer's id?",
+                validate: validateNum,
             },
             {
                 type: "input",
                 name: "engineerEmail",
-                message: "What is your engineer's email?"
+                message: "What is your engineer's email?",
+                validate: validateEmail,
             },
             {
                 type: "input",
                 name: "engineerGithub",
-                message: "What is your engineer's github?"
+                message: "What is your engineer's github?",
+                validate: validateGithub,
             },
             {
                 type: "confirm",
@@ -107,22 +137,26 @@ function appMenu() {
             {
                 type: "input",
                 name: "internName",
-                message: "What is your intern's name?"
+                message: "What is your intern's name?",
+                validate: validateName,
             },
             {
                 type: "input",
                 name: "internId",
-                message: "What is your intern's id?"
+                message: "What is your intern's id?",
+                validate: validateNum,
             },
             {
                 type: "input",
                 name: "internEmail",
-                message: "What is your intern's email?"
+                message: "What is your intern's email?",
+                validate: validateEmail,
             },
             {
                 type: "input",
                 name: "internSchool",
-                message: "What is your intern's school?"
+                message: "What is your intern's school?",
+                validate: validateSchool,
             }
         ]).then(answers => {
             const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
